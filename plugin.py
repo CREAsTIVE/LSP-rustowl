1fd5447c8fdefab2e8a03496a7a3da8e66719d26
CREAsTIVE <alexshveev@gmail.com>
2025-05-30 07:38:46 +0500

    feat: logic splited to commands

--- plugin.py
+++ plugin.py
@@ -9,9 +9,7 @@ from sublime import Region
 
 import LSP
 from LSP.plugin.core.views import position, position_to_offset, point_to_offset, offset_to_point
-from LSP.plugin import AbstractPlugin
-from LSP.plugin import Request
-from LSP.plugin import register_plugin, unregister_plugin
+from LSP.plugin import AbstractPlugin, LspTextCommand, Request, register_plugin, unregister_plugin
 from LSP.plugin.core.sessions import Session
 
 from urllib.parse import urljoin, quote
@@ -149,24 +147,6 @@ def plugin_unloaded() -> None:
 # Plugin logic:
 
 
-def _session_by_name(view, name: str):
-    target = name
-    listener = _get_listener(view)
-    if listener:
-        for sv in listener.session_views_async():
-            if sv.session.config.name == target:
-                return sv.session
-    return None
-
-
-def _get_listener(view: sublime.View):
-    return LSP.plugin.core.registry.windows.listener_for_view(view)
-
-
-def get_session(view) -> Session:
-    return _session_by_name(view, SESSION_NAME)
-
-
 def path_to_uri(file_path: str) -> str:
     path = Path(file_path).resolve().absolute().as_posix()
 
@@ -187,41 +167,52 @@ def get_setting(view: Optional[sublime.View], key: str, default: Optional[Union[
     return settings.get(key, default)
 
 
-class Listener(sublime_plugin.EventListener):
-    def on_selection_modified(self, view: sublime.View):
-        if get_setting(view, "rustowl.hover_type") == "cursor" and len(view.sel()) > 0:
-            self.request_analyze(view, position(view, view.sel()[0].a))
+class LspRustowlCommand(LspTextCommand):
+    session_name = SESSION_NAME
 
-    def on_hover(self, view, point, hover_zone):
-        if get_setting(view, "rustowl.hover_type") == "mouse":
-            self.request_analyze(view, position(view, point))
 
-    def request_analyze(self, view: sublime.View, position: int):
-        session = get_session(view)
-        if (session is None):
-            return
-
-        debug(f"sending request for {position}...")
+class LspRustowlClearCommand(LspRustowlCommand):
+    def run(self, edit):
+        debug("clearing highliting...")
 
         for type in colors.keys():
-            view.erase_regions("rustowl-"+type)
+            self.view.erase_regions("rustowl-"+type)
+
+
+class LspRustowlAnalyzeCommand(LspRustowlCommand):
+    def run(self, edit: sublime.Edit, point: Optional[int] = None):
+        debug(f"analyze triggered with point: {point}")
+
+        if point is None:
+            point = self.view.sel()[0].a
+
+        session = self.session_by_name(self.session_name)
+        if session is None:
+            return
+
+        lsp_pos = position(self.view, point)
+        uri = path_to_uri(self.view.file_name())
+
+        self.view.run_command("lsp_rustowl_clear")
+
+        debug(f"sending request for pos: {lsp_pos}, file: '{uri}'...")
 
         session.send_request(
             Request(
                 "rustowl/cursor",
                 {
-                    "position": position,
+                    "position": lsp_pos,
                     "document": {
-                        "uri": path_to_uri(view.file_name())
+                        "uri": uri
                     }
                 }
             ),
-            lambda x: self.on_result(view, x),
+            lambda x: self.on_result(self.view, x),
             lambda x: print(x)
         )
 
     def on_result(self, view: sublime.View, result):
-        debug(f"result recived, status: {result['status']}")
+        debug(f"analyze result recived, status: {result['status']}")
 
         # Status update
         view.set_status("rustowl-status", "rustowl status: " + result["status"])
@@ -253,3 +244,13 @@ class Listener(sublime_plugin.EventListener):
                 annotation_color=colors[type][1],
                 flags=sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE | sublime.DRAW_SOLID_UNDERLINE
             )
+
+
+class Listener(sublime_plugin.EventListener):
+    def on_selection_modified(self, view: sublime.View):
+        if get_setting(view, "rustowl.hover_type") == "cursor" and len(view.sel()) > 0:
+            view.run_command("lsp_rustowl_analyze", {"point": view.sel()[0].a})
+
+    def on_hover(self, view, point, hover_zone):
+        if get_setting(view, "rustowl.hover_type") == "mouse":
+            view.run_command("lsp_rustowl_analyze", {"point": point})
